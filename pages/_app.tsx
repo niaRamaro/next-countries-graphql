2d78ed20ed235cdb28d6074341da655abd3cb818
niaRamaro <ramarosandynia@gmail.com>
2020-11-05 13:54:04 +0100

    Change font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import 'flag-icon-css/css/flag-icon.min.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
 export default MyApp
